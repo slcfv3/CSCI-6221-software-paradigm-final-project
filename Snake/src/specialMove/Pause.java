cc09b8ba45b761cc7196c390cd514fb89ee29033
Ziyi <slcfv3@gmail.com>
2017-04-22 21:08:19 -0400

    Little changes

--- Snake/src/specialMove/Pause.java
+++ Snake/src/specialMove/Pause.java
@@ -3,10 +3,12 @@ package specialMove;
 public class Pause extends Special{
 	
 	
-	@Override
+	
 	public void specialMove(int interval, double changeRate, boolean pauseState) {
 		// TODO Auto-generated method stub
-		pauseState = !pauseState;
+		
+	    pauseState = !pauseState;
+		
 	}
 
 }
--- Snake/src/specialMove/SpeedUP.java
+++ Snake/src/specialMove/SpeedUP.java
@@ -2,11 +2,15 @@ package specialMove;
 
 public class SpeedUP extends Special {
 
+	int interval;
+	double changeRate;
 	
 	@Override
 	public void specialMove(int interval, double changeRate, boolean pauseState) {
 		// TODO Auto-generated method stub
+		
 		interval *= changeRate; 
+		
 	}
 
 }
